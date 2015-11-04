e68d0160a1a1c01a1839855a5c8b3cca20295313
gabriela29 <gabrielaauqui@upeu.edu.pe>
2015-11-03 19:30:19 -0500

    ood

--- app/src/main/java/upeu/edu/pe/MainActivity.java
+++ app/src/main/java/upeu/edu/pe/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Presentar", Snackbar.LENGTH_LONG)//ola
+                Snackbar.make(view, "Presentar", Snackbar.LENGTH_LONG)//ola jjjjjjjjjjjj
                         .setAction("Action", null).show();
             }
         });
